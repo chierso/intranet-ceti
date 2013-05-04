8949a5be6fe80781350be2cbc39bb6223b58098b
delmon <delmonxd@gmail.com>
2013-05-03 21:07:03 -0500

    Agregando Controllers

---
+++ application/controllers/abm/abm_alumn.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	class abm_alumn extends CI_Controller {
+	
+	public function __construct()
+      {
+         parent::__construct();
+		 $this->load->library('grocery_CRUD');
+      }
+	
+	public function index()
+	{
+		
+	}
+		
+	public function registrarAlumno()
+	{		
+		$persona		=	$this->input->post('tipo_persona',TRUE)."";
+		$nombres		=	$this->input->post('txt_nombres',TRUE)."";
+		$apellidos		=	$this->input->post('txt_apellidos',TRUE)."";
+		$direccion		=	$this->input->post('txt_direccion',TRUE)."";
+		$dni			=	$this->input->post('txt_dni',TRUE)."";
+		$telefono		=	$this->input->post('txt_telefono',TRUE)."";
+		$celular		=	$this->input->post('txt_celular',TRUE)."";
+		$sexo			=	$this->input->post('rbt_sexo',TRUE)."";
+		$nacimiento		=	$this->input->post('txt_fec_nacimiento',TRUE)."";
+		$correo			=	$this->input->post('txt_email',TRUE)."";
+		$grado			=	$this->input->post('cbx_grado',TRUE)."";
+		$seccion		=	$this->input->post('rbt_seccion',TRUE)."";
+	    $this->load->model("abm/abm_alumno_model");
+	    $data=$this->abm_alumno_model->registrarAlumno($nombres, 
+											    $apellidos, 
+											    $direccion, 
+											    $dni, 
+											    $telefono, 
+											    $celular, 
+											    $sexo, 
+											    $nacimiento, 
+											    $correo, 
+											    $grado, 
+											    $seccion
+	    	);
+	    $this->output->set_content_type('json')->set_output(json_encode($data));	
+	}
+}
