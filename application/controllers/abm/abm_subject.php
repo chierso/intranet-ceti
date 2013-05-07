ef02dbeefb100b52e82a5c63efa491ab2ad23c07
unknown <delmonxd@gmail.com>
2013-05-07 22:32:44 +0200

    Agregando db

--- application/controllers/abm/abm_subject.php
+++ application/controllers/abm/abm_subject.php
@@ -15,7 +15,13 @@
 	public function listarCurso()
 	{
 		//$this->grocery_crud->set_theme('twitter-bootstrap');
-		$this->grocery_crud->set_table('tbl_subject');
+		//$this->grocery_crud->set_theme('datatables');
+		$this->grocery_crud->set_table('tbl_subject_registration');
+		$this->grocery_crud->display_as('id_subject','Curso');
+		$this->grocery_crud->set_subject('Curso');
+		$this->grocery_crud->add_fields('id_subject','year_section');
+		$this->grocery_crud->field_type('id_subject', 'multiselect');
+		$this->grocery_crud->set_relation('id_subject','tbl_subject','name');
 		$output = $this->grocery_crud->render();
 		$this->_example_output($output);
 	}
